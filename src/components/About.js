6ab6e43fe6c0901c8ed62482ef5890eda8185b4b
Terry Yegon <yegonterry618@gmail.com>
2025-07-14 18:43:41 +0300

    import and export lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { image } from "../data/user"; // ✅ import image from user.js
 
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <h2>About</h2>
+      <img src={image} alt="Profile" />
     </div>
   );
 }
+
+export default About; // ✅ default export
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar"; // ✅ Case-sensitive and uses relative path
+
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Navbar />
       <Home />
       <About />
     </div>
